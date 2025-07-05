3f30e38126cf757040e6036c84811d3fa85fe181
gabistam <gabistam@gmail.com>
2025-07-05 09:36:11 +0200

    remove example .env file and clean up database and task model code

--- models/Task.js
+++ models/Task.js
@@ -1,15 +1,11 @@
-const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
   // TODO: Définir les champs de la tâche
   // titre: { type: ???, required: ??? },
   // description: { type: ??? },
   // statut: { type: ???, enum: ['a_faire', 'terminee'], default: 'a_faire' },
   // dateEcheance: { type: ??? }
-}, {
+  
   // TODO: Ajouter les timestamps automatiques
   // timestamps: ???
-});
 
 // TODO: Exporter le modèle
-// module.exports = mongoose.model('???', taskSchema);
